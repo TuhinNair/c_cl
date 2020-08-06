a11eb45084c0ac2f8fa349a00f707e6738ad0a5d
Tuhin Nair <tuhinnair444@gmail.com>
2020-08-06 18:18:27 +0700

    Add successful shortest path base test cases'

--- test_routes.py
+++ test_routes.py
@@ -3,13 +3,11 @@ from routes import Routes, InvalidRouteError
 
 
 class GraphInitializationTest(unittest.TestCase):
-    def setUp(self):
-        self.routes = Routes.load_routes('test_csv_files/test_routes_1.csv')
-
     def test_successful_graph_initialization(self):
-        self.assertEqual(self.routes.graph.edges['A'], ['B', 'C'])
-        self.assertEqual(self.routes.graph.edges['C'], ['F'])
-        self.assertEqual(self.routes.graph.edges['B'], [])
+        routes = Routes.load_routes('test_csv_files/test_routes_1.csv')
+        self.assertEqual(routes.graph.edges['A'], ['B', 'C'])
+        self.assertEqual(routes.graph.edges['C'], ['F'])
+        self.assertEqual(routes.graph.edges['B'], [])
 
     def test_failed_csv_lex(self):
         with self.assertRaises(IndexError):
@@ -18,7 +16,7 @@ class GraphInitializationTest(unittest.TestCase):
     def test_failed_csv_parse(self):
         with self.assertRaises(ValueError):
             Routes.load_routes('test_csv_files/wrong_distance_format.csv')
-    
+
     def test_failed_illogical_route_distances(self):
         with self.assertRaises(InvalidRouteError):
             Routes.load_routes('test_csv_files/negative_distance.csv')
@@ -26,6 +24,20 @@ class GraphInitializationTest(unittest.TestCase):
             Routes.load_routes('test_csv_files/impossible_cycle.csv')
         with self.assertRaises(InvalidRouteError):
             Routes.load_routes('test_csv_files/impossible_unique_nodes.csv')
+
+
+class ShortestPathTest(unittest.TestCase):
+    def test_shortest_path_base_case(self):
+        routes = Routes.load_routes('test_csv_files/base_case.csv')
+        res = routes.shortest_path('A', 'A')
+        self.assertEqual(res.path, ['A'])
+        self.assertEqual(res.dist, 0)
+
+    def test_simple_shortest_path(self):
+        routes = Routes.load_routes('test_csv_files/base_case.csv')
+        res = routes.shortest_path('A', 'T')
+        self.assertEqual(res.path, ['A', 'B', 'T'])
+        self.assertEqual(res.dist, 2)
 
 
 if __name__ == '__main__':
